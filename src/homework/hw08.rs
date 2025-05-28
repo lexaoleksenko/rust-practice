72d73ac28c33c2a2678dfc504bff50f58f70174f
Oleksiy Mikolayovich <oleksenko.siy@gmail.com>
2025-05-28 10:29:56 +0300

    hw08

---
+++ src/homework/hw08.rs
@@ -0,0 +1,20 @@
+fn is_prime(n: u32) -> bool {
+    if n < 2 {
+        return false;
+    }
+    if n == 2 {
+        return true;
+    }
+    if n % 2 == 0 {
+        return false;
+    }
+
+    let limit = (n as f64).sqrt() as u32;
+    for i in (3..=limit).step_by(2) {
+        if n % i == 0 {
+            return false;
+        }
+    }
+
+    true
+}
