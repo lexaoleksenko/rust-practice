dea8720d2620e3c3f3839bfaeca1717ba50f842a
Oleksiy Mikolayovich <oleksenko.siy@gmail.com>
2025-05-28 09:55:32 +0300

    hr-task01

--- src/hackerrank/task01.rs
+++ src/hackerrank/task01.rs
@@ -1,4 +1,21 @@
-#[test]
-fn test1() {
-    assert_eq!(1 + 1, 2);
+// https://www.hackerrank.com/challenges/simple-array-sum/problem
+
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    input.clear();
+
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    let sum: i32 = input
+        .trim()
+        .split_whitespace()
+        .map(|x| x.parse::<i32>().expect("Not a number"))
+        .sum();
+
+    println!("{}", sum);
 }
